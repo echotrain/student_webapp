15c0f9d46dedca985e0d89b00c7b9f10cfb00fff
Josh Anderson <joshuabair22@gmail.com>
2017-03-16 16:47:34 -0600

    refactored: studentsFileSplit.js, changed 'var' to 'let'

--- server/studentsFileSplit.js
+++ server/studentsFileSplit.js
@@ -1,14 +1,14 @@
 // load modules
-var fs = require('fs');
+let fs = require('fs');
 
 // execute
 console.log('Splitting students.json file into individual student files.');
 
-var fileName = process.argv[2];
-var students = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+let fileName = process.argv[2];
+let students = JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
-for (var stu in students) {
-    var studentID = students[stu].id;
+for (let stu in students) {
+    let studentID = students[stu].id;
     delete students[stu].id;
     fs.writeFile(`students/${studentID}.json`, JSON.stringify(students[stu], null, '\t'), 'utf8');
 }

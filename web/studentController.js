5dadb4d42c7e7d7f34454981b6b7371a19026c6b
Josh Anderson <joshuabair22@gmail.com>
2017-03-21 15:35:37 -0600

    adding angular material libraries and starting angular conversion. added 'studentService.js' for serving up REST api calls for student data.

--- web/studentController.js
+++ web/studentController.js
@@ -3,3 +3,18 @@
  */
 
 //TODO: angularize all the things...
+angular.module('app').controller('studentController', function($scope, $mdDialog, studentSrv, $filter, $timeout) {
+
+    //variables
+    $scope.students = [];
+
+    //AJAX json
+    function init() {
+        studentSrv.getStudents().then(function (result) {
+            $scope.students = result.data;
+
+        });
+    }
+
+
+});
